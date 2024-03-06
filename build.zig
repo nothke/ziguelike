b0ed9786ded7dda40684861f16276694bf967481
nothke <nothke@gmail.com>
2024-03-06 02:11:38 +0100

    Reading raw input, drew a little "screen"

--- build.zig
+++ build.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -11,6 +10,8 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+
+    exe.linkLibC();
 
     b.installArtifact(exe);
 
@@ -27,14 +28,6 @@ pub fn build(b: *std.Build) void {
 
     // TESTS
 
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/root.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
     const exe_unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
@@ -44,6 +37,5 @@ pub fn build(b: *std.Build) void {
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,38 @@
 const std = @import("std");
+const c = @cImport({
+    @cInclude("conio.h");
+});
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
+    const stdin = std.io.getStdIn().reader();
+    _ = stdin; // autofix
+
+    try stdout.print("Run `zig build test` to run the tests.", .{});
     try stdout.print("Run `zig build test` to run the tests.\n", .{});
 
-    try bw.flush(); // don't forget to flush!
+    while (true) {
+        for (0..10) |_| {
+            try stdout.print("\n", .{});
+            for (0..30) |_| {
+                try stdout.print("#", .{});
+            }
+        }
+
+        try bw.flush();
+
+        const input: i32 = c.getch();
+
+        try stdout.print("\n ------------------------- \n", .{});
+
+        if (input == 'q')
+            break;
+    }
 }
 
 test "simple test" {
